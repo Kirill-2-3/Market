ef7e7b2e16d5503b71a268bfd895d64b10397db6
Kirill K <87173384+Kirill-2-3@users.noreply.github.com>
2023-06-18 14:49:26 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+const currentUrl = window.location.pathname;
+const indexUrl = currentUrl.indexOf('.');
+const pathname = currentUrl.slice(1, indexUrl);
+
+const activeElems = document.querySelectorAll('[data-active]');
+const navElem = document.querySelector(`[data-element="${pathname}"]`);
+
+if (navElem) {
+  const nameHeader = navElem.dataset.header;
+  const elemHeader = document.querySelector(`[data-active="${nameHeader}"]`);
+
+  elemHeader.classList.add('active');
+}
+
+activeElems.forEach(activeElem => {
+  const nameElem = activeElem.dataset.active;
+
+  if (pathname === nameElem) {
+    activeElem.classList.add('active');
+  }
+});
+
+const documentWidth = document.documentElement.offsetWidth;
+
+if (documentWidth < 1050) {
+  alert('Ваше устройство не поддерживается! Перейдите на ПК версию');
+}
+
+window.addEventListener(`resize`, event => {
+  const documentWidth = document.documentElement.offsetWidth;
+
+  if (documentWidth < 1050) {
+    alert('Ваше устройство не поддерживается! Перейдите на ПК версию');
+  }
+}, false);
